999046be381acbdfa47601fa7a815ace625bea4b
rush-shale <hinoyogjohnrrushel@gmail.com>
2024-10-05 12:41:18 +0800

    added initial files

--- applet.js
+++ applet.js
@@ -1,29 +1,34 @@
 class DataLogger {
     constructor(buttonId, cardContainerId, clearButtonId, logCountId) {
-        this.logButton = document.getElementById(buttonId);
-        this.cardContainer = document.getElementById(cardContainerId);
-        this.clearButton = document.getElementById(clearButtonId);
-        this.logCountElement = document.getElementById(logCountId); 
+        this.logButton = document.getElementById(buttonId); 
+        this.cardContainer = document.getElementById(cardContainerId); 
+        this.clearButton = document.getElementById(clearButtonId); 
+        this.logCountElement = document.getElementById(logCountId);  
         this.loggedData = [];
 
+       
         this.logButton.addEventListener('click', () => this.logData());
         this.clearButton.addEventListener('click', () => this.clearLogs());
     }
 
+   
     logData() {
-        const timestamp = new Date().toLocaleString();
-        this.loggedData.push(timestamp);
-        this.updateCardContainer();
-    }
-
-    clearLogs() {
-        this.loggedData = []; 
+        const timestamp = new Date().toLocaleString();  
+        this.loggedData.push(timestamp);  
         this.updateCardContainer(); 
     }
 
-    updateCardContainer() {
-        this.cardContainer.innerHTML = '';
+    
+    clearLogs() {
+        this.loggedData = [];  
+        this.updateCardContainer();  
+    }
 
+    
+    updateCardContainer() {
+        this.cardContainer.innerHTML = '';  
+
+      
         this.loggedData.forEach(data => {
             const card = document.createElement('div');
             card.className = 'card mb-2';
@@ -33,21 +38,25 @@ class DataLogger {
                     <p class="card-text">${data}</p>
                 </div>
             `;
-            this.cardContainer.appendChild(card);
+            this.cardContainer.appendChild(card);  
         });
-        
+
+       
         this.displayLogCount();
     }
 
+    
     countLogs() {
         return this.loggedData.length;
     }
 
+   
     displayLogCount() {
-        const logCount = this.countLogs();
-        this.logCountElement.innerHTML = `<p>Total Logs: ${logCount}</p>`; 
+        const logCount = this.countLogs(); 
+        this.logCountElement.innerHTML = `Total Logs: ${logCount}`;  
     }
 }
+
 
 document.addEventListener('DOMContentLoaded', () => {
     new DataLogger('logButton', 'cardContainer', 'clearButton', 'logCount'); 
